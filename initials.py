9ab15a0412888f5a8849981d4a6c994797f960d3
samuelops <appiahkubisamuel210@gmail.com>
2026-05-31 10:02:42 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Prints my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Appiah-Kubi Samuel Obeng
+# STUDENT ID: 2425401187
+# Email: appiahkubisamuel210@gmail.com
+# Grader: Augustus
+#
+# On my honor, Appiah-Kubi Samuel Obeng, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in small and large format.
 
+    print()
+    print("...AKSO")
+    print()
+
+    print("...AAAAAAAAAAAA........KKK......KKK........SSSSSSSSSSSS........OOOOOOOOOO.....")
+    print("...AAAA....AAAA........KKK.....KKK.........SSSSSSSSSSSS........OOO....OOO.....")
+    print("...AAAA....AAAA........KKK....KKK..........SSSS................OOO....OOO.....")
+    print("...AAAA....AAAA........KKK...KKK...........SSSS................OOO....OOO.....")
+    print("...AAAAAAAAAAAA........KKKKKKKK............SSSSSSSS............OOO....OOO.....")
+    print("...AAAA....AAAA........KKKKKKKK...............SSSSSS...........OOO....OOO.....")
+    print("...AAAA....AAAA........KKK...KKK..................SSSS.........OOO....OOO.....")
+    print("...AAAA....AAAA........KKK....KKK.................SSSS.........OOO....OOO.....")
+    print("...AAAA....AAAA...**...KKK.....KKK....**...SSSSSSSSSSSS...**...OOO....OOO...**")
+    print("...AAAA....AAAA...**...KKK......KKK...**...SSSSSSSSSSSS...**...OOOOOOOOOO...**")
+
+    print()
 
 
 main()
